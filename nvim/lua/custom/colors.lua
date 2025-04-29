734efb2de7e4c5975ff40f26521a9675f5cfef98
dinoDanic <dino.danic@gmail.com>
2025-04-29 21:46:38 +0200

    fish

--- nvim/lua/custom/colors.lua
+++ nvim/lua/custom/colors.lua
@@ -15,4 +15,4 @@ vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'none' })
 vim.api.nvim_set_hl(0, 'VertSplit', { bg = 'none' })
 vim.api.nvim_set_hl(0, 'SignColumn', { bg = 'none' })
 vim.api.nvim_set_hl(0, 'StatusLine', { bg = 'none' })
-vim.api.nvim_set_hl(0, 'TabLineFill', { bg = 'none' })
+
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,7 +3,7 @@ local wezterm = require("wezterm")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
-local theme = wezterm.plugin.require('https://github.com/neapsix/wezterm').main
+local theme = wezterm.plugin.require("https://github.com/neapsix/wezterm").main
 
 -- Appearancec
 config.font = wezterm.font("JetBrains Mono", { weight = "Bold", italic = false })
@@ -13,10 +13,10 @@ config.window_background_opacity = 0.8
 config.macos_window_background_blur = 100
 config.hide_tab_bar_if_only_one_tab = true
 config.window_padding = {
-  left = 20,
-  right = 20,
-  top = 20,
-  bottom = 0,
+	left = 20,
+	right = 20,
+	top = 20,
+	bottom = 0,
 }
 
 -- For example, changing the color scheme:
@@ -25,22 +25,22 @@ config.window_frame = theme.window_frame()
 
 -- open link
 config.keys = {
-  {
-    key = "l",
-    mods = "CMD",
-    action = wezterm.action.QuickSelectArgs {
-      label = "Open URL",
-      patterns = { "https?://\\S+" },
-      action = wezterm.action_callback(function(window, pane)
-        local url = window:get_selection_text_for_pane(pane)
-        if url and url:match("^https?://") then
-          wezterm.open_with(url)
-        else
-          window:toast_notification("WezTerm", "No valid URL found", nil, 4000)
-        end
-      end),
-    },
-  },
+	{
+		key = "l",
+		mods = "CMD",
+		action = wezterm.action.QuickSelectArgs({
+			label = "Open URL",
+			patterns = { "https?://\\S+" },
+			action = wezterm.action_callback(function(window, pane)
+				local url = window:get_selection_text_for_pane(pane)
+				if url and url:match("^https?://") then
+					wezterm.open_with(url)
+				else
+					window:toast_notification("WezTerm", "No valid URL found", nil, 4000)
+				end
+			end),
+		}),
+	},
 }
 
 -- and finally, return the configuration to wezterm
