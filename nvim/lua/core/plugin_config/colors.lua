2c6745cc77d7b5b538b601630eae7c4e7d45123e
Dino Danic <dino.danic@gmail.com>
2025-09-10 10:21:31 +0200

    add nvim

---
+++ nvim/lua/core/plugin_config/colors.lua
@@ -0,0 +1,42 @@
+-- vim.o.termguicolors = true
+-- Colorize
+-- require 'colorizer'.setup({
+--   '*',
+-- }, { mode = 'foreground', names = false })
+
+-- vim.cmd [[ colorscheme gruvbox ]]
+-- vim.cmd [[ colorscheme night-owl ]]
+-- vim.cmd [[ colorscheme catppuccin ]]
+-- vim.cmd [[ colorscheme catppuccin-frappe ]]
+-- vim.cmd [[colorscheme solarized-osaka]]
+-- vim.cmd [[ colorscheme nord ]]
+-- vim.cmd [[ colorscheme nightfox ]]
+vim.cmd [[ colorscheme rose-pine-main ]]
+-- vim.cmd [[ colorscheme nightfly ]]
+-- vim.cmd [[ colorscheme tokyonight ]]
+-- vim.cmd("colorscheme kanagawa")
+-- vim.cmd("colorscheme onedark")
+--
+
+-- opacity transparent
+-- vim.cmd "au ColorScheme * hi Normal ctermbg=none guibg=none"
+-- vim.cmd "au ColorScheme * hi SignColumn ctermbg=none guibg=none"
+-- vim.cmd "au ColorScheme * hi NormalNC ctermbg=none guibg=none"
+-- vim.cmd "au ColorScheme * hi MsgArea ctermbg=none guibg=none"
+-- vim.cmd "au ColorScheme * hi TelescopeBorder ctermbg=none guibg=none"
+-- vim.cmd "au ColorScheme * hi NvimTreeNormal ctermbg=none guibg=none"
+-- vim.cmd "let &fcs='eob: '"
+-- vim.cmd "au ColorScheme * hi NonText ctermbg=none guibg=none"
+-- vim.cmd "au ColorScheme * hi Pmenu ctermbg=none guibg=none"
+-- vim.cmd "au ColorScheme * hi FloatBorder ctermbg=none guibg=none"
+-- vim.cmd "au ColorScheme * hi NormalFloat ctermbg=none guibg=none"
+-- vim.cmd "au ColorScheme * hi TabLine ctermbg=none guibg=none"
+
+
+-- vim.cmd [[ colorscheme catppuccin-mocha ]]
+-- vim.cmd [[ colorscheme carbonfox ]]
+-- vim.cmd [[ colorscheme tokyonight-storm ]]
+-- vim.cmd [[ colorscheme nightfox ]]
+-- vim.cmd [[ colorscheme catppuccin-macchiato ]]
+-- vim.cmd [[ colorscheme catppuccin-frappe ]]
+-- vim.cmd [[ colorscheme cyberdream ]]
