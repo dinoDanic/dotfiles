848f7a0c21711e6a9f07996d82fc054870fe56a3
dinoDanic <dino.danic@gmail.com>
2025-11-20 07:22:58 +0100

    Speed up fades, disable swipe, set <leader><leader> to git status

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -190,7 +190,10 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       -- vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
-      vim.keymap.set('n', '<leader><leader>', '<cmd>Telescope buffers sort_mru=true sort_lastused=true theme=ivy<cr>', { desc = '[ ] Find existing buffers' })
+      -- vim.keymap.set('n', '<leader><leader>', '<cmd>Telescope buffers sort_mru=true sort_lastused=true theme=ivy<cr>', { desc = '[ ] Find existing buffers' })
+      vim.keymap.set('n', '<leader><leader>', '<cmd>Telescope git_status theme=ivy<cr>', {
+        desc = '[ ] Show edited git files',
+      })
 
       -- It's also possible to pass additional configuration options.
       --  See `:help telescope.builtin.live_grep()` for information about particular keys
