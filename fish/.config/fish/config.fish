2e09d790b77d69b1e355c499b5af72c40b5afac8
Dino Danic <dino.danic@gmail.com>
2025-12-19 12:21:07 +0100

    fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -28,56 +28,6 @@ if status is-interactive
         end
     end
 
-    # ----------------------------- TMUX -------------------------------
-
-    function develop
-        tmux -neww session_name: -n window_name -d
-    end
-
-    # -------------------------- AEROSPACE -----------------------------
-
-    if test -d (brew --prefix)"/share/fish/completions"
-        set -p fish_complete_path (brew --prefix)/share/fish/completions
-    end
-
-    if test -d (brew --prefix)"/share/fish/vendor_completions.d"
-        set -p fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
-    end
-
-    # ----------------------------- PATHS ------------------------------
-
-    # react native
-    set -gx ANDROID_HOME $HOME/Library/Android/sdk
-    set -gx PATH $PATH $ANDROID_HOME/emulator
-    set -gx PATH $PATH $ANDROID_HOME/platform-tools
-
-    # flutter
-    set -gx PATH Users/dino/development/flutter/bin $PATH
-
-    function flutter
-        fvm flutter $argv
-    end
-
-    set -gx GOPATH /Users/dino/go
-
-    # ----------------------------- AEROSPACE ------------------------------
-    function sf
-        aerospace list-windows --all | fzf --bind 'enter:execute(bash -c "aerospace focus --window-id {1}")+abort'
-    end
-
-    # ----------------------------- ALIAS ------------------------------
-
-    # swag
-    alias swag=$GOPATH/bin/swag
-
-    # air
-    alias air=$GOPATH/bin/air
-
-    # templ
-    alias templ=$GOPATH/bin/templ
-
-    # lunarvim 
-    alias lvim="/Users/dino/.local/bin/lvim"
 
     # neovim
     alias nc="cd ~/.config/nvim && nvim"
@@ -88,9 +38,6 @@ if status is-interactive
 
     # alacritty 
     alias ac="nvim ~/.config/alacritty/alacritty.toml"
-
-    # kitty 
-    alias kc="nvim ~/.config/kitty/kitty.conf"
 
     # fish 
     alias fc="nvim ~/.config/fish/config.fish"
@@ -125,16 +72,6 @@ function y
     end
     rm -f -- "$tmp"
 end
-# ----------------------------- YAZI ------------------------------
-
-# Setting PATH for Python 3.11
-# The original version is saved in /Users/dino/.config/fish/config.fish.pysave
-set -x PATH "/Library/Frameworks/Python.framework/Versions/3.11/bin" "$PATH"
-
-# pnpm
-set -gx PNPM_HOME /Users/dino/Library/pnpm
-set -gx PATH "$PNPM_HOME" $PATH
-# pnpm end
 
 # add  default editor
 set -g EDITOR nvim
